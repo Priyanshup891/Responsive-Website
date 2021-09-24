624e326e6b2eedc94b7b12cc7dc4d4707d86eadf
PriyanshuPatil007 <priyanshupatil806>
2021-09-24 17:32:03 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let menu = document.getElementById("menu");
+
+        function closemenu(){
+            menu.style.left = "-100%";
+        }
+
+        function openmenu(){
+            menu.style.left = "0";
+        }
